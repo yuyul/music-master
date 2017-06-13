8c30d0bc20954681a2226e79326f7d3f408fca7a
Juan Julian Caro Sancho <Juan Julian Caro Sancho>
2017-06-13 07:54:43 +0200

    add fetch to search artist

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Profile from './Profile';
 import './App.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: ''
+            query: '',
+            artist: null
         }
     }
 
     search() {
         console.log('this.state', this.state);
+        const myHeaders = new Headers();
+        myHeaders.append('Authorization', 'Bearer ');
+
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        console.log('FETCH_URL', FETCH_URL);
+        fetch(FETCH_URL, {
+            method: 'GET',
+            headers: myHeaders
+        })
+        .then(response => response.json())
+        .then(json =>{
+            const artist = json.artists.items[0];
+            console.log(artist);
+            this.setState({artist});
+        });
     }
 
     render() {
@@ -36,10 +54,9 @@ class App extends Component {
                         </InputGroup.Addon>
                     </InputGroup>
                 </FormGroup>
-                <div className="Profile">
-                    <div>Artist Picture</div>
-                    <div>Artist Name</div>
-                </div>
+                <Profile
+                    artist={this.state.artist}
+                />
                 <div className="Gallery">
                     Gallery
                 </div>
